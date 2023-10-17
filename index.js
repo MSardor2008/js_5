033caff408498e244f946aadd3d498e6a0002912
MSardor2008 <147912029+MSardor2008@users.noreply.github.com>
2023-10-17 15:15:54 +0500

    Add files via upload

---
+++ index.js
@@ -0,0 +1,27 @@
+// 1
+
+let nic = prompt("What is your name ?")
+if (nic.length % 2 === 0) {
+    alert ('Welcome')
+}
+else{
+    alert ('Sorry, your name has to be countable')
+}
+
+// 2
+
+let number = prompt ("What is your favourite number?")
+if (number % 2 === 0 ) {
+    alert ('it is countable')
+} else {
+    alert ('it is uncountable')
+}
+
+// 3
+
+let inf = prompt ("What is your father's name ?")
+if (inf === inf.includes ('a')) {
+    alert ('you won IPHONE 15.')
+} else {
+    alert ('okay, thank you for information.')
+}
